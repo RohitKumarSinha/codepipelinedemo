cbb7a3166394afd6e475d8b8a8326c64776aad52
RKSINHA <sinharohit3333@gmail.com>
2023-02-15 12:37:44 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <h1>my name is rohit kumar sinha</h1>
   </React.StrictMode>
 );
 
